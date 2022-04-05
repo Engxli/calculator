f4240490735eff391a9d4288877727e652b9eb4b
Ali Ahmad <turgut@Alis-MacBook-Pro.local>
2022-04-06 00:25:06 +0300

    1.0 && 1 is not the same :)

--- calculator.py
+++ calculator.py
@@ -1,8 +1,21 @@
 
+from ast import operator
+from curses.ascii import isdigit
+
+
 def main():
 	#write your code here
-	pass
-	
+
+	firstNum = input("Enter the first number: ")
+	secondNum = input("Enter the seoncd number: ")
+	operation = input("Choose the operation (+, -, /, *): ")
+
+	if (operation in "+*-/" and firstNum.isdigit() and secondNum.isdigit() ):
+		ans = f"{str(firstNum)}{operation}{str(secondNum)}"
+		print("The answer is: ", str(round(eval(ans))))
+	else:
+		print("the operation is not valid")
+
 
 
 
